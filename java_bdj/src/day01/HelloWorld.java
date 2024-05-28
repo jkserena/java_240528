e87cc6f7400d3931e0f2fd47786c0c1112856fca
bdj.dreamer <jkserena@naver.com>
2024-05-28 17:10:51 +0900

    day1 java example helloworld

---
+++ java_bdj/src/day01/HelloWorld.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class HelloWorld {
+
+	/** method 설명을 위한 주석
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// sysout -> ctrl+space + enter 
+
+		System.out.printf("hello world!\n");
+		System.out.println("hello world!");
+		
+	}
+
+}
