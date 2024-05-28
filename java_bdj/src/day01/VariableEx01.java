4800591a271046aea13a02882c036a6232def60f
bdj.dreamer <jkserena@naver.com>
2024-05-28 17:11:07 +0900

    day1 java variable example

---
+++ java_bdj/src/day01/VariableEx01.java
@@ -0,0 +1,27 @@
+package day01;
+
+public class VariableEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch1,ch2;
+		ch2 = 'A';
+		System.out.println(ch2);
+		// System.out.println(v);
+		char ch3 = '+', ch4 = '\\';
+		System.out.println(ch3);
+		System.out.println(ch4);
+		// System.out.printf("%c %c",ch3, ch4);
+		long l = 123_456_789_12____3L;
+		System.out.println(l);
+		System.out.printf("[%-25d]\n",l);
+		
+		float f = 3.14159265321f;
+		double d = 3.14159265321;
+		System.out.println(d);
+		
+		boolean isEvan = true;
+		
+	}
+
+}
