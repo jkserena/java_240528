4a4894455bab1d9b2a91685971e390e0bbd37f75
bdj.dreamer <jkserena@naver.com>
2024-05-29 15:04:38 +0900

    Update O_AssignmentOperatorEx01.java

--- java_bdj/src/day02/O_AssignmentOperatorEx01.java
+++ java_bdj/src/day02/O_AssignmentOperatorEx01.java
@@ -3,7 +3,10 @@ package day02;
 public class O_AssignmentOperatorEx01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int num1 = 10, num2 = 20;
+		
+		num1 += num2;
 
 	}
 
